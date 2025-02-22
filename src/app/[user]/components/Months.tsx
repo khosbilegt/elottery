c3a93a1835f6201eaeaf0a65bdef22b08410dda6
Khosbilegt Bilegsaikhan <archerdoc13@gmail.com>
2025-02-23 00:01:18 +0800

    fixed dockerfile and minor bug fix

--- src/app/[user]/components/Months.tsx
+++ src/app/[user]/components/Months.tsx
@@ -1,10 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation';
-import { Button, FloatButton, List, Form, DatePicker, Modal, Typography } from 'antd';
-import type { DatePickerProps } from 'antd';
-import { UserAddOutlined } from '@ant-design/icons';
-import { MonthRow } from '.';
-
+import React, { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import {
+  Button,
+  FloatButton,
+  List,
+  Form,
+  DatePicker,
+  Modal,
+  Typography,
+} from "antd";
+import type { DatePickerProps } from "antd";
+import { UserAddOutlined } from "@ant-design/icons";
+import { MonthRow } from ".";
 
 const { Title } = Typography;
 
@@ -12,31 +19,33 @@ function Months() {
   const [months, setMonths] = useState<{ [key: string]: any }>({});
   const [monthNames, setMonthNames] = useState<string[]>([]);
   const [isDatePickerOpen, setDatePickerOpen] = useState(false);
-  const [date, setDate] = useState('');
+  const [date, setDate] = useState("");
 
-  const path = usePathname().replace('/', '');
+  const path = usePathname().replace("/", "");
 
-  const onChange: DatePickerProps['onChange'] = (dateTemp, dateString) => {
-    setDate(dateString);
-  }
+  const onChange: DatePickerProps["onChange"] = (dateTemp, dateString) => {
+    if (typeof dateString === "string") {
+      setDate(dateString);
+    }
+  };
 
   const showDatePickerModal = () => {
     setDatePickerOpen(true);
-  }
+  };
 
   const handleDatePickerOk = () => {
-    const cachedJson = localStorage.getItem('elottery') || '{}';
+    const cachedJson = localStorage.getItem("elottery") || "{}";
     let newJson = JSON.parse(cachedJson);
 
-    if(newJson[path].months[date] != null) {
+    if (newJson[path].months[date] != null) {
     } else {
       newJson[path].months.push({
         month: date,
-        receipts: []
-      })
+        receipts: [],
+      });
     }
 
-    localStorage.setItem('elottery', JSON.stringify(newJson));
+    localStorage.setItem("elottery", JSON.stringify(newJson));
     window.location.reload();
     setDatePickerOpen(false);
   };
@@ -46,56 +55,74 @@ function Months() {
   };
 
   useEffect(() => {
-    const cachedJson: { [key: string]: any } = JSON.parse(localStorage.getItem('elottery') || '{}');
-    
+    const cachedJson: { [key: string]: any } = JSON.parse(
+      localStorage.getItem("elottery") || "{}"
+    );
+
     if (cachedJson[path].months != null) {
-      setMonths(cachedJson[path].months)
+      setMonths(cachedJson[path].months);
     }
   }, []);
 
   useEffect(() => {
     if (months != null) {
-      const updatedMonthNames = Object.keys(months).map((key) => months[key].month);
+      const updatedMonthNames = Object.keys(months).map(
+        (key) => months[key].month
+      );
       setMonthNames(updatedMonthNames);
     }
   }, [months]);
 
-
   useEffect(() => {
     if (months != null) {
-      const updatedMonthNames = Object.keys(months).map((key) => months[key].month);
+      const updatedMonthNames = Object.keys(months).map(
+        (key) => months[key].month
+      );
       setMonthNames(updatedMonthNames);
     }
   }, [isDatePickerOpen]);
 
   return (
-    <div className='w-1/3 h-full text-center flex flex-col'>
-      <List 
-        className='text-center'
-        header={<Title className='font-medium'>{path.replace('/', '')}</Title>}
+    <div className="w-1/3 h-full text-center flex flex-col">
+      <List
+        className="text-center"
+        header={<Title className="font-medium">{path.replace("/", "")}</Title>}
         dataSource={monthNames}
         renderItem={(item) => (
           <List.Item>
             {/* <Button mark className='w-full' onClick={showModal}>{item}</Button> */}
-            <MonthRow name={item}/>
+            <MonthRow name={item} />
           </List.Item>
         )}
       />
-      <FloatButton type='primary' onClick={showDatePickerModal} icon={<UserAddOutlined />} className='w-[75px] h-[75px]'/>
-      <Modal 
-        title="Хэрэглэгч үүсгэх" 
+      <FloatButton
+        type="primary"
+        onClick={showDatePickerModal}
+        icon={<UserAddOutlined />}
+        className="w-[75px] h-[75px]"
+      />
+      <Modal
+        title="Хэрэглэгч үүсгэх"
         open={isDatePickerOpen}
         onCancel={handleDatePickerCancel}
-        footer={<Button className='btn primary' onClick={handleDatePickerOk}>Үүсгэх</Button>}
+        footer={
+          <Button className="btn primary" onClick={handleDatePickerOk}>
+            Үүсгэх
+          </Button>
+        }
       >
         <Form>
           <Form.Item label="Баримтын сарыг сонгоно уу: ">
-            <DatePicker onChange={onChange} picker="month" className='w-full text-center'/>
+            <DatePicker
+              onChange={onChange}
+              picker="month"
+              className="w-full text-center"
+            />
           </Form.Item>
         </Form>
       </Modal>
     </div>
-  )
+  );
 }
 
 export default Months;
